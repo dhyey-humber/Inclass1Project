aee446b675367982739eba7800b16f5d604020d0
dhyey-humber <55444226+dhyey-humber@users.noreply.github.com>
2019-10-01 16:49:16 -0400

    Splash Screen

---
+++ app/src/main/java/com/ceng319/inclass1project/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.ceng319.inclass1project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
